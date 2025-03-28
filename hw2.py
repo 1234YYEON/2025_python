adf66d4ea88c79ccddf582a17396a663ca83f351
1234YYEON <foxan3363@gmail.com>
2025-03-28 14:28:58 +0900

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,26 @@
+# 사용자 정의 함수부
+def exchange(amount):
+    n500 = amount//500
+    amount %= 500
+
+    n100 = amount//100
+    amount %= 100
+
+    n50 = amount//50
+    amount %= 50
+
+    n10 = amount//10
+    amount %= 10
+
+    print("500원 동전의 개수: ", n500)
+    print("100원 동전의 개수: ", n100)
+    print("50원 동전의 개수: ", n50)
+    print("10원 동전의 개수: ", n10)
+
+def get_integer():
+    return int(input("동전으로 교환하고자 하는 금액은? "))
+
+# 주 프로그램부
+
+amount = get_integer()
+exchange(amount)
